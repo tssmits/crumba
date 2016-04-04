2f3f8e937e9e66be7cc084d352f1f956346a88a0
Thijs Smits <beyanora@gmail.com>
2016-04-04 18:00:46 +0200

    semantic ui added

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function () {
-  gulp.src('app/sass/style.scss')
+  gulp.src('./app/sass/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./web/css'));
+});
+
+gulp.task('copybower', function () {
+  gulp
+    .src(['angular/angular.min.js',
+      'angular/angular.min.js.map',
+      'jquery/dist/jquery.slim.min.js',
+      'jquery/dist/jquery.slim.min.map',
+      'lodash/dist/lodash.min.js'],
+      { 'cwd': 'bower_components' })
+    .pipe(gulp.dest('./web/scripts/lib'))
 });
 
 gulp.task('default', function () {
---
+++ web/scripts/app/app.js
@@ -0,0 +1,4 @@
+;(function ($,_,angular,window,document,undefined) {
+'use strict';
+
+}(jQuery,_,angular,window,document));
