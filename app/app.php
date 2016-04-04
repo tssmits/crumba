7e52b6d59f8e9843b7123089370fd8d2db1cc91e
Thijs Smits <beyanora@gmail.com>
2016-04-04 16:45:54 +0200

    okay, twig hello world working

--- app/app.php
+++ app/app.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace crumba;
-
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
@@ -9,15 +7,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/crumba.php';
 require_once __DIR__ . '/data.php';
 
-// $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
-// $dotenv->load();
+$dotenv = new \Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
 
 class MyApplication extends \Silex\Application {
   use \Silex\Application\TwigTrait;
 }
 
 $app = new MyApplication;
-$app['debug'] = true;
+$app['debug'] = !!getenv('DEBUG');
 
 // Register the monolog logging service
 $app->register(new \Silex\Provider\MonologServiceProvider(), array(
@@ -29,12 +27,6 @@ $app->register(new \Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/views'
 ));
 
-$app = new MyApplication();
-
 $app->get('/', function () use ($app) {
-  // return 'derp';
-  // return $app->render('html.twig');
-  // return $app['twig']->render('html.twig');
+  return $app['twig']->render('index.twig');
 });
-
-$crumba = new Crumba();
--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once __DIR__ . '/app.php';
+require_once __DIR__ . '/../app/app.php';
 
 $app->run();
